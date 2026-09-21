ba2a213df31a0f91b64e66dc0cd82524395efa68
Phạm Quang Vinh <98261160+toanp5287@users.noreply.github.com>
2026-09-22 00:21:44 +0700

    fix cors for vercel frontend

--- travel-booking-backend/src/app.js
+++ travel-booking-backend/src/app.js
@@ -18,7 +18,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://booking-tour-cvmxu1wxp-vinhabc.vercel.app",
+    ],
     credentials: true,
   }),
 );
